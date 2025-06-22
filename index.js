11613b7d67a6602bb64d1c4981266b0a07314a46
Yeshu <yeshu6221@gmail.com>
2025-06-22 15:48:09 +0545

    fix cors

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
